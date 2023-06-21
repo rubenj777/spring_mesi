26523e9897006fd52f605b8c3367fcebeaf96ceb
JALLIFIER-TALMAT Ruben <ruben.jallifier@gmail.com>
2023-06-21 08:12:09 +0200

    first commit

---
+++ src/test/java/com/project/mesi/MesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
